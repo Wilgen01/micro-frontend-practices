e0314091787804e754885fc6bc90c02e9d0eea21
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-10-18 18:03:23 -0500

    add task form

--- list/src/app/app.component.ts
+++ list/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'list';
+  public fb = inject(FormBuilder);
+  public form: FormGroup;
+
+  constructor() {
+    this.form = this.fb.group({
+      title: ["", Validators.required]
+    })
+  }
+
+  public onSubmit(){
+    console.log(this.form.value);
+    
+  }
+
+
 }
